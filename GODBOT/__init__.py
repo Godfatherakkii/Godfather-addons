dc2b6da5e2b05aed0cc6709b39b5457867d51148
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-26 08:37:15 -0700

    Update __init__.py

--- GODBOT/__init__.py
+++ GODBOT/__init__.py
@@ -4,7 +4,7 @@ import time
 
 import heroku3
 
-from .Config import Config
+from .GODBOT import GODBOT
 from .core.logger import logging
 from .core.session import legend
 from .helpers.utils.utils import runasync
