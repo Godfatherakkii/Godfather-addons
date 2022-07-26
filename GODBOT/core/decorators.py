52f5c9237f3be59b2a6250332f32ff415ebddf82
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-26 09:09:13 -0700

    Update decorators.py

--- GODBOT/core/decorators.py
+++ GODBOT/core/decorators.py
@@ -3,8 +3,8 @@ import asyncio
 from telethon.errors import FloodWaitError, MessageNotModifiedError
 from telethon.events import CallbackQuery
 
-from ..Config import Config
-from ..sql_helper.globals import gvarstatus
+from .Config import Config
+from .sql_helper.globals import gvarstatus
 
 
 def check_owner(func):
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "Only My Master can Access This Button !!\n\nDeploy your own @LegendBot_XD."
+                or "Only My Master can Access This Button !!\n\nDeploy your own @LEGENDHACKER_IIN."
             )
             await c_q.answer(
                 HELP_TEXT,
